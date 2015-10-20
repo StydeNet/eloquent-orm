ac4bc37bf0c88678fb2c5bc7cf72d5a8ff362a68
Rimorsoft Online <me@rimorsoft.com>
2015-10-20 18:53:30 -0500

    CRUD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,30 +12,45 @@
 */
 
 use EloquentORM\User;
+use Faker\Factory as Faker;
 
 Route::get('/create', function () {
+    $faker = Faker::create();
     
     $user = User::create([
-        'name' => 'Lynda Morales',
-        'email' => 'l@italomoralesf.com',
+        'name' => $faker->name,
+        'email' => $faker->email,
         'password' => bcrypt('123456'),
-        'gender' => 'f',
-        'biography' => 'Alumna de PHP'
+        'gender' => $faker->randomElement(['f', 'm']),
+        'biography' => $faker->text(255)
     ]);
     
-    return 'Usuario Guardado';
+    return $user;
     
 });
 
-Route::get('/update-user', function () {
+Route::get('/read/{id}', function ($id){
+    $user = User::find($id);
+    return $user;
+});
+
+Route::get('/update/{id}', function ($id) {
+    $faker = Faker::create();
+    $user = User::find($id);
     
-    $user = User::find(1);
-    
-    $user->gender = 'm';
-    $user->biography = 'Profesor de Programación';
+    $user->name = $faker->name;
+    $user->gender = $faker->randomElement(['f', 'm']);
+    $user->biography = $faker->text(255);
     
     $user->save();
     
-    return 'Usuario Actualizado';
+    return $user;
     
+});
+
+Route::get('/delete/{id}', function ($id){
+    $user = User::find($id);
+    $user->delete();
+    
+    return 'Usuario Eliminado';
 });
