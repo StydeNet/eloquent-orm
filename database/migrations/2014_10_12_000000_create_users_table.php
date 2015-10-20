54e41f7e9910d93392bc8a570539fe863a5cb68b
Rimorsoft Online <me@rimorsoft.com>
2015-10-20 15:48:14 -0500

    Config Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->enum('gender', ['f', 'm']);
+            $table->string('biography');
             $table->rememberToken();
             $table->timestamps();
         });
